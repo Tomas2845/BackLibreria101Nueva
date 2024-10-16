e0ae3cc428b2aa0d91b1d380fcea5b3323b638d5
tomacho <tomacho28.th@gmail.com>
2024-10-16 11:51:12 -0300

    terminamos de configurar pruebas, enviamos mensaje desde back

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-import express from 'express'
+import express from "express";
 
-console.log("hola mundo")
+console.log("hola mundo");
+//1 - configurar un puerto
 
+const app = express();
 
-//1 - configurar un puerto 
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), () => {
+  console.info("Estoy escuchando el puerto" + app.get("port"));
+});
 
-//2 - configurar middlewares 
+//2 - configurar middlewares
 
-//3 - configurar las rutas 
+//3 - configurar las rutas
+
+// http://localhost:4000/prueba
+
+app.get("/prueba", (req, res) => {
+  console.log("alguien hizo una solicitud get a la ruta de prueba ");
+  res.send("Hola mundo desde backend");
+});
