7b10adf8e4929fa42708e8d38f2dcab5ad0be4f6
Tomacho <tomacho28.th@gmail.com>
2024-10-18 09:24:33 -0300

    dividimos la logica de las rutas y controladores

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,7 +1,5 @@
 import { Router } from "express";
 import { funcionPrueba } from "../controllers/productos.controllers.js";
-
 const router = Router();
 router.route("/prueba").get(funcionPrueba);
-
 export default router 
